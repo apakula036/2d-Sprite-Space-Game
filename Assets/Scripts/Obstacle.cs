c7ab98945b247222219bb26088660401be801f76
Andrew <70990567+apakula036@users.noreply.github.com>
2026-02-18 20:41:47 -0800

    added new obstacles

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -2,13 +2,28 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    float minSize = 0.5f;
-    float maxSize = 2.0f;
+    public float minSize = 0.5f;
+    public float maxSize = 2.0f;
+    public float minSpeed = 50f;
+    public float maxSpeed = 150f;
+    public float maxSpinSpeed = 10f;
+
+    Rigidbody2D rb;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         float randomSize = Random.Range(minSize, maxSize);
-        transform.localScale = new Vector3 (randomSize , randomSize , 1);
+        transform.localScale = new Vector3(randomSize, randomSize, 1);
+
+        float randomSpeed = Random.Range(minSpeed, maxSpeed) / randomSize;
+        Vector2 randomDirection = Random.insideUnitCircle;
+
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(randomDirection * randomSpeed);
+
+        float randomTorque = Random.Range(-maxSpinSpeed, maxSpinSpeed);
+        rb.AddTorque(randomTorque);
     }
 
     // Update is called once per frame
