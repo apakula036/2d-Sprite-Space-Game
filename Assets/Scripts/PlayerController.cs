bc96412e534616a3490717b3fd0de9615f283cbd
Andrew <70990567+apakula036@users.noreply.github.com>
2026-02-23 20:30:10 -0800

    added restart button and particle effects

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,44 +10,69 @@ public class PlayerController : MonoBehaviour
     private float elapsedTime = 0f;
     public float thrustForce = 2f;
     public GameObject BoosterFlmae;
-    Rigidbody2D rb;
+    Rigidbody2D spaceship;
     public UIDocument uiDocument;
     private Label scoreText;
+    private Button restartButton;
+    public GameObject explosionEggfect; 
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        //initialize the variables to update these items
+        spaceship = GetComponent<Rigidbody2D>();
         scoreText = uiDocument.rootVisualElement.Q<Label>("ScoreLabel");
+        restartButton = uiDocument.rootVisualElement.Q<Button>("RestartButton");
+        restartButton.style.display = DisplayStyle.None;
+        restartButton.clicked += ReloadScene;
     }
 
     // Update is called once per frame
     void Update()
     {
-        scoreText.text = "Score: " + score;
+        UpdateScore();
+        MovePlayer();
+        ActivateFlame();
+    }
+    void UpdateScore()
+    {
+        // Add the score number to the UI
         elapsedTime += Time.deltaTime;
         score = Mathf.FloorToInt(elapsedTime * scoreMultiplier);
-        Debug.Log("Score: " + score);
+        scoreText.text = "Score: " + score;
+    }
+    void OnCollisionEnter2D(Collision2D collision) 
+    {
+        Destroy(gameObject);   
+        Instantiate(explosionEggfect, transform.position, transform.rotation);
+        restartButton.style.display = DisplayStyle.Flex;
+    }
+    void ReloadScene()
+    {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    void MovePlayer()
+    {
+        //Check if mouse1 is pressed 
         if (Mouse.current.leftButton.isPressed) {
+
             // Calculate mouse direction
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.value);
-            
             Vector2 direction = (mousePos - transform.position).normalized;
 
             // Move player in direction of mouse
             transform.up = direction;
-            rb.AddForce(direction * thrustForce);
+            spaceship.AddForce(direction * thrustForce);
         }
-
+    }
+    void ActivateFlame()
+    {
         if (Mouse.current.leftButton.wasPressedThisFrame){
             BoosterFlmae.SetActive(true);
-            Debug.Log("FLAME ON ");
         }
         else if (Mouse.current.leftButton.wasReleasedThisFrame){
             BoosterFlmae.SetActive(false);
         }
-    }
-    void OnCollisionEnter2D(Collision2D collision) 
-    {
-    Destroy(gameObject);       
     }
 }
